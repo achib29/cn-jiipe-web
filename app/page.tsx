63df5b582a03f48e300168fa1780141c35d7ec68
achib29 <123372604+achib29@users.noreply.github.com>
2025-05-23 14:34:23 +0700

    add special economic zone

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/hero-section";
 import AboutSection from "@/components/about-section";
+import SpecialEconomicZoneSection from "@/components/SpecialEconomicZoneSection";
 import FacilitiesSection from "@/components/facilities-section";
 import LocationSection from "@/components/location-section";
 import ContactSection from "@/components/contact-section";
