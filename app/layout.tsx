23fbfe7075be2edc5f4a494ffc3de1d215e201d9
Moh. Abdul Khasib <abdul.khasib@bkms.jiipe.co.id>
2025-06-20 12:39:27 +0700

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,10 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'JIIPE - 古能工业园官网',
-  description: 'Strategic industrial port estate in East Java, Indonesia offering world-class infrastructure and facilities for industrial and logistics operations.',
+  description:
+    'Strategic industrial port estate in East Java, Indonesia offering world-class infrastructure and facilities for industrial and logistics operations.',
   icons: {
-    icon: '/jiipe-favicon.png', //
+    icon: '/jiipe-favicon.png',
   },
 };
 
@@ -21,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="zh" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
@@ -33,6 +34,16 @@ export default function RootLayout({
           <main>{children}</main>
           <Footer />
         </ThemeProvider>
+
+        {/* ChatSimple Widget */}
+        <chat-bot
+          platform_id="0633efa7-7842-4c57-be89-04ecd02cb3a3"
+          user_id="5c60a06c-b262-4c9f-8a3d-225f0a49f8a0"
+          chatbot_id="c4625f04-a66a-4877-8c7d-deb8a32869c7"
+        >
+          <a href="https://www.chatsimple.ai/?utm_source=widget&utm_medium=referral">chatsimple</a>
+        </chat-bot>
+        <script src="https://cdn.chatsimple.ai/chat-bot-loader.js" defer></script>
       </body>
     </html>
   );
