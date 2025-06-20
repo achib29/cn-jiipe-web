07a6956b7537c6ab954b24f2ef4f3638bd0f829d
Moh. Abdul Khasib <abdul.khasib@bkms.jiipe.co.id>
2025-06-20 12:55:11 +0700

    Add Boot

--- types/custom-elements.d.ts
+++ types/custom-elements.d.ts
@@ -1,5 +1,10 @@
 declare namespace JSX {
   interface IntrinsicElements {
-    'chat-bot': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    'chat-bot': {
+      platform_id: string;
+      user_id: string;
+      chatbot_id: string;
+      children?: React.ReactNode;
+    };
   }
 }
