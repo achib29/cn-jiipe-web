41b16e4cb7d1cf25a7f75a20b131b480c2213ceb
achib29 <123372604+achib29@users.noreply.github.com>
2025-08-05 17:54:55 +0700

    merubah Baidu tag

--- components/contact-section.tsx
+++ components/contact-section.tsx
@@ -87,7 +87,7 @@ export default function ContactSection() {
       .then((res) => {
         if (res.ok) {
           if (window._agl) {
-            window._agl.push(["track", ["success", { t: 3 }]]);
+            window._agl.push(['track', ['success', { t: 3 }]]);
             console.log('Baidu tracking triggered');
           }
           router.push("/thank-you");
